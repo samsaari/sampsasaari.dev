4cf47448e222927e4d9dd9cdaaf0ee4ec0667768
Sampsa Saari <sampsa.saari@gmail.com>
2021-12-20 15:32:56 +0200

    Add react helmet to template

--- src/templates/template.js
+++ src/templates/template.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Helmet from "react-helmet"
 import { GlobalStyle } from '../styled/styles'
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Helmet htmlAttributes={{ lang: 'en' }}>
+          <title>Sampsa Saari</title>
+          <meta name="description" content="Web developer based in Oulu, Finland." />
+      </Helmet>
       <GlobalStyle />
-        <main>{children}</main>
+      <main>{children}</main>
       {/* <Footer /> */}
     </>
   )
